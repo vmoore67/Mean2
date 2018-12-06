7d602620fbfd883492193493d1999845dd8b9971
Virginia Moore <vmoore0607@yahoo.com>
2018-12-06 16:57:43 -0500

    Sync vs Async

---
+++ readFileAsync.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+console.log("Going to get a file");
+fs.readFile('readFileSync.js', function(err, file) {
+	console.log("Got the file");
+	});
+
+
+console.log("App continues...");
